1211a034aa794d374f1c0eebe7b1177ed07aea6e
_ReD_ <apple.tyomka@gmail.com>
2017-06-26 04:39:29 +0300

    Complete login activity & registration activity

--- app/src/main/java/com/innopolis/android/androidstudents/ManagerActivity.java
+++ app/src/main/java/com/innopolis/android/androidstudents/ManagerActivity.java
@@ -1,10 +1,8 @@
 package com.innopolis.android.androidstudents;
 
-import android.content.Intent;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
